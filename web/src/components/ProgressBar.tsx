434cb3a58514add598fe467fc9426ceec038df4f
Raphael Berger <raphaelmb@gmail.com>
2023-01-19 13:29:21 -0300

    Frontend followup

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgressbarProps {
+  progress: number;
+}
+
+export function Progressbar(props: ProgressbarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{
+          width: `${props.progress}%`,
+        }}
+      />
+    </div>
+  );
+}
